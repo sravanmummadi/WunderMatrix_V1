8c0fc429e211f5da1339941f69f805f91ed1ce26
sravanmummadi <sravan.mummadi06@gmail.com>
2016-11-13 17:32:40 -0600

    todoitemsservice

--- app/src/Todo_Matrix/Todo_Item_List/todo-item-list.component.ts
+++ app/src/Todo_Matrix/Todo_Item_List/todo-item-list.component.ts
@@ -1,58 +1,22 @@
 import {Component} from 'angular2/core';
 import {TodoItemComponent} from '../Todo_Item/todo-item.component';
+import {TodoItemService} from '../todo-item.service';
+
 
 @Component({
     selector: 'todo-item-list',
     directives: [TodoItemComponent],
+    providers: [TodoItemService],
     templateUrl: './app/src/Todo_Matrix/Todo_Item_List/todo-item-list.component.html',
     //styleUrls: ['./app/src/Todo_Item_List/todo-item-list.component.css']
 })
 export class TodoItemListComponent {
 
+    constructor(private todoItemService: TodoItemService) {};
+    ngOnInit() {
+        this.todoItems = this.todoItemService.get();
+    };
     
+    todoItems;
     
-    todoItems = [
-        {
-            id: 1,
-            name: "Firebug",
-            medium: "Series",
-            category: "Science Fiction",
-            year: 2010,
-            watchedOn: 1294166565384,
-            isFavorite: false
-        },
-        {
-            id: 2,
-            name: "The Small Tall",
-            medium: "Movies",
-            category: "Comedy",
-            year: 2015,
-            watchedOn: null,
-            isFavorite: true
-        }, {
-            id: 3,
-            name: "The Redemption",
-            medium: "Movies",
-            category: "Action",
-            year: 2016,
-            watchedOn: null,
-            isFavorite: false
-        }, {
-            id: 4,
-            name: "Hoopers",
-            medium: "Series",
-            category: "Drama",
-            year: null,
-            watchedOn: null,
-            isFavorite: true
-        }, {
-            id: 5,
-            name: "Happy Joe: Cheery Road",
-            medium: "Movies",
-            category: "Action",
-            year: 2015,
-            watchedOn: 1457166565384,
-            isFavorite: false
-        }
-    ];
 }
