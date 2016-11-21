f9b6a896a3ef625961a1dbd1351edbef114e0b70
sravanmummadi <sravan.mummadi06@gmail.com>
2016-11-20 18:24:00 -0600

    missed commit

--- app/src/bootstrap.ts
+++ app/src/bootstrap.ts
@@ -1,4 +1,4 @@
 import {bootstrap} from 'angular2/platform/browser';
 import {AppComponent} from './app.component';
-import {TodoItemService} from './Todo_Matrix/todo-item.service';
+
 bootstrap(AppComponent);
