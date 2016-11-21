21414b053c88893217c7d80f273233f4b890c57b
sravanmummadi <sravan.mummadi06@gmail.com>
2016-11-20 18:23:13 -0600

    added service at component level

--- app/src/app.component.ts
+++ app/src/app.component.ts
@@ -1,7 +1,7 @@
 import {Component} from 'angular2/core';
 import {TodoQuadrant} from './Todo_Matrix/Quadrant/quad.component';
 import {TodoItemFormComponent} from './Todo_Matrix/Todo_Input/todo-input.component';
-
+import {TodoItemService} from './Todo_Matrix/todo-item.service';
 import {TodoItemListComponent} from './Todo_Matrix/Todo_Item_List/todo-item-list.component';
 
 
@@ -9,6 +9,7 @@ import {TodoItemListComponent} from './Todo_Matrix/Todo_Item_List/todo-item-list
     selector: 'Wunder-Matrix-app',
     directives: [TodoQuadrant,TodoItemFormComponent,TodoItemListComponent],
     templateUrl: './app/src/app.component.html',
+    providers: [TodoItemService],
     styleUrls: ['./app/src/app.component.css'],
     
 })
