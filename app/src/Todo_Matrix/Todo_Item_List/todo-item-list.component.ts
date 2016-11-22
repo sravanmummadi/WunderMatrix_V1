945c9765d38aa8101948cc4638ffd52497f1f588
sravanmummadi <sravan.mummadi06@gmail.com>
2016-11-21 22:10:00 -0600

    pipe integrated

--- app/src/Todo_Matrix/Todo_Item_List/todo-item-list.component.ts
+++ app/src/Todo_Matrix/Todo_Item_List/todo-item-list.component.ts
@@ -1,20 +1,21 @@
 import {Component, Input} from 'angular2/core';
 import {TodoItemComponent} from '../Todo_Item/todo-item.component';
 import {TodoItemService} from '../todo-item.service';
-
+import {CategoryListPipe} from '../category-list.pipe';
 
 
 
 @Component({
     selector: 'todo-item-list',
     directives: [TodoItemComponent],
-    
+    pipes: [CategoryListPipe],
+
     //providers: [TodoItemService],
     templateUrl: './app/src/Todo_Matrix/Todo_Item_List/todo-item-list.component.html'
     //styleUrls: ['./app/src/Todo_Item_List/todo-item-list.component.css']
 })
 export class TodoItemListComponent {
-   
+   @Input('category') category;
 
     constructor(private todoItemService1: TodoItemService) { };
     ngOnInit() {
---
+++ app/src/Todo_Matrix/category-list.pipe.ts
@@ -0,0 +1,16 @@
+import {Pipe} from 'angular2/core';
+
+@Pipe({
+    name: 'categoryList'
+})
+export class CategoryListPipe {
+    transform(mediaItems,category) {
+        var mediaItems_filtered = [];
+        mediaItems.forEach(mediaItem => {
+            if (mediaItem.category==category)  {
+                mediaItems_filtered.push(mediaItem);
+            }
+        });
+        return mediaItems_filtered;
+    }
+}
